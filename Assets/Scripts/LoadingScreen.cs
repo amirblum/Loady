8db7fb0c1b9771ebaf4c363ad91671661c3db777
Amir Blum <amirblum@gmail.com>
2020-11-27 19:22:57 +0200

    Loading screen tips

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField] LoadingSequence _testSequence;
+
+    [Header("Config")] 
+    [SerializeField] float _nextTipButtonFadeInTime;
+
+    [Header("Elements")]
+    [SerializeField] TMP_Text _loadingTip;
+    [SerializeField] Button _nextTipButton;
+
+    protected void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            _nextTipButton.onClick.Invoke();
+        }
+    }
+
+    public void StartSequence(LoadingSequence loadingSequence)
+    {
+        StartCoroutine(SequenceCoroutine(loadingSequence));
+    }
+
+    private IEnumerator SequenceCoroutine(LoadingSequence loadingSequence)
+    {
+        Debug.Log($"Starting sequence: {loadingSequence.SequenceName}");
+        foreach (var tip in loadingSequence.TipSequence)
+        {
+            Debug.Log($"Applying tip text {tip.TipText}");
+            yield return DisplayTipCoroutine(tip);
+            yield return FadeInNextTipButton();
+            yield return WaitUntilNextTipButtonPressed();
+            _nextTipButton.gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator DisplayTipCoroutine(LoadingSequence.LoadingTip tip)
+    {
+        var waitBetweenLetters = 1f / tip.ScrollSpeed;
+        
+        for (var i = 0; i < tip.TipText.Length; i++)
+        {
+            var substring = tip.TipText.Substring(0, i + 1);
+            _loadingTip.text = substring;
+            yield return new WaitForSeconds(waitBetweenLetters); 
+        }
+    }
+
+    private IEnumerator WaitUntilNextTipButtonPressed()
+    {
+        var nextPressed = false;
+        Action waitForNextPressed = () => nextPressed = true;
+        _nextTipButton.onClick.AddListener(waitForNextPressed.Invoke);
+        yield return new WaitUntil(() => nextPressed);
+        _nextTipButton.onClick.RemoveListener(waitForNextPressed.Invoke);
+    }
+
+    private IEnumerator FadeInNextTipButton()
+    {
+        _nextTipButton.gameObject.SetActive(true);
+        var canvasGroup = _nextTipButton.GetComponent<CanvasGroup>();
+        
+        for (var time = 0f; time < _nextTipButtonFadeInTime; time += Time.deltaTime)
+        {
+            var percent = time / _nextTipButtonFadeInTime;
+            canvasGroup.alpha = percent;
+            yield return null;
+        }
+    }
+
+    [Button]
+    private void RunTestSequence()
+    {
+        StartSequence(_testSequence);
+    }
+}
---
+++ Assets/Scripts/LoadingSequence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LoadingSequence", menuName = "LoadingSequence", order = 1)]
+public class LoadingSequence : ScriptableObject
+{
+    [Serializable]
+    public class LoadingTip 
+    {
+        public float PercentToLoad;
+        public string TipText;
+        public float ScrollSpeed = 50;
+        public Sprite Emoji;
+        public bool WaitForNextPressed = true;
+    }
+
+    public string SequenceName;
+    public LoadingTip[] TipSequence;
+}
